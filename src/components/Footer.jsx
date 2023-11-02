ec671549e9569d432718a241ca132b41c9bf6ed4
blendar-opgny <blendar@opgny.com>
2023-11-02 22:59:16 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -94,19 +94,18 @@ export function Footer() {
   return (
     <Container as="footer" className="mt-24 w-full sm:mt-32 lg:mt-40">
       <FadeIn>
-        <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
-          <Navigation />
-          <div className="flex lg:justify-end">
-            <NewsletterForm />
-          </div>
-        </div>
         <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
-          <Link href="/" aria-label="Home">
+                  <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
+          <Navigation />
+        </div>
+        <div>
+          <Link href="/" aria-label="Home" className="mb-3 flex justify-end">
             <Logo className="h-8" fillOnHover />
           </Link>
-          <p className="text-sm text-neutral-700">
+          <p className="mt-4 text-sm text-neutral-700">
             © Dimensio Agency LLC, {new Date().getFullYear()}
           </p>
+        </div>
         </div>
       </FadeIn>
     </Container>
